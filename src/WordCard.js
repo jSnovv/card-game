c7728b699977497394d805530cc8f2d9462ed885
Amnee Hima <amneehima@gmail.com>
2022-08-13 14:25:11 +0700

    Add handler

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,9 +1,12 @@
 import CharacterCard from "./CharacterCard";
 import React from "react";
 export default function WordCard(props){
+    const activationHandler = c => { console.log(`${c} has been activated.`) }
+
     return (
         <div>
-            {Array.from(props.value).map((c, i) => <CharacterCard value={c} key={i}/>)}
+            {Array.from(props.value).map((c, i) => 
+                <CharacterCard value={c} key={i} activationHandler={activationHandler}/>)}
         </div>
     );
 }
